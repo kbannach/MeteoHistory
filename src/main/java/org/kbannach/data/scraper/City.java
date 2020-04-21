7fb1a77a5cf0de606ced1c2a7f60d62c72ac0d3a
kbannach <krzysztof92b@gmail.com>
2020-04-21 23:51:12 +0200

    added City.GDANSK

--- src/main/java/org/kbannach/data/scraper/City.java
+++ src/main/java/org/kbannach/data/scraper/City.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum City {
-    GDYNIA("http://www.meteo.pl/um/php/meteorogram_id_um.php?ntype=0u&id=1878");
+    GDYNIA("http://www.meteo.pl/um/php/meteorogram_id_um.php?ntype=0u&id=1878"),
+    GDANSK("http://www.meteo.pl/um/php/meteorogram_list.php?ntype=0u&fdate=2020042112&row=346&col=210&lang=pl&cname=Gda%F1sk");
 
     private final String meteorogramUrl;
 }
